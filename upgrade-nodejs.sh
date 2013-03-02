e2e9de64c5f060ffb3362b9eec24a86b0818ba2a
Guilherme Rodrigues <guilherme@vtex.com.br>
2013-03-02 10:20:24 -0300

    Upgrade node script

---
+++ upgrade-nodejs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo 'Uninstalling earliar NodeJS version'
+sudo npm rm npm -g
+rm -rf /usr/local/bin/node /usr/local/bin/node-waf /usr/local/include/node /usr/local/lib/node /usr/local/lib/pkgconfig/nodejs.pc /usr/local/share/man/man1/node.1.gz
+tar -zxf node-v0.8.21.tar.gz
+echo 'Node.js unpack completed'
+# Install Node.js
+echo 'Install Node.js'
+cd node-v0.8.21
+./configure && make && checkinstall --install=yes --pkgname=nodejs --pkgversion "0.8.21" --default
+echo 'Node.js 0.8.21 install completed'
